404db03124bb3130373567f067533a74986f18c5
Hendri Pretorius <pretorh@gmail.com>
2015-05-14 21:07:23 +0200

    use getopts to parse command line arguments
    -r for remote is required
    -d for db version (optional)

--- list.sh
+++ list.sh
@@ -1,6 +1,26 @@
 set -e
 
-remote=${1?"need remote user@ip to install to"}
 dbVersion=90
+
+usage() {
+    echo "-r user@remote [-d dbVersion]" 1>&2
+    exit 1;
+}
+
+while getopts "d:r:" o; do
+    case "${o}" in
+        d)
+            dbVersion=${OPTARG}
+            ;;
+        r)
+            remote=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+[ -z $remote ] && echo "Need user@remote to connect to (-r)" 1>&2 && usage
 
 ssh $remote /opt/kwpc/list.sh $dbVersion
