634f845bc25130d93dd8342835dc89c04158b687
Hendri Pretorius <pretorh@gmail.com>
2015-05-14 20:58:25 +0200

    ignore xargs exit code 123
    123 is returned when some of xargs' command failed
    this is expected, since some of the files might not be on the device
    anymore

--- client/list.sh
+++ client/list.sh
@@ -1,7 +1,11 @@
-set -e
-
 dbVersion=${1?"need db version"}
 b=`dirname $0`
 
 sqlite3 ~/.kodi/userdata/Database/MyVideos$dbVersion.db < $b/$dbVersion.sql \
     | xargs -L 1 -I filename ls "filename" 2>/dev/null
+exitCode=$?
+
+# ignore error code 123 (some instances of ls failed, which is expected)
+if [ $exitCode -ne 123 ] ; then
+    exit $exitCode
+fi
