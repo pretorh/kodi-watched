47326e90b5ac5298016b0d7d3808ddc4cea9a136
Hendri Pretorius <pretorh@gmail.com>
2015-06-12 21:39:03 +0200

    check if the command file exists

--- client/client.sh
+++ client/client.sh
@@ -1,4 +1,11 @@
 b=`dirname $0`
 cmd=${1?"need command as first parameter"}
 
-"$b/$cmd.sh" "${@:2}"
+if [ -f "$b/$cmd.sh" ] ; then
+    ext=sh
+else
+    echo "unknown command (no file matching $cmd)" 2>&1
+    exit 1
+fi
+
+"$b/$cmd.$ext" "${@:2}"
